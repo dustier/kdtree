a572fa198eaa66f7f529586dd98d0493804d76d5
dustier <dustier@foxmail.com>
2023-01-19 08:04:47 +0800

    quick select

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,40 @@
 
-pub(crate) fn quick_select() -> usize {
-    0
+pub(crate) fn quick_select<T: std::cmp::PartialEq + std::cmp::PartialOrd + Copy>(
+    data: &Vec<T>,
+    indices: &mut Vec<usize>,
+    l: usize,
+    r: usize,
+    k: usize,
+) -> T {
+    if l >= r {
+        return data[indices[l]];
+    }
+
+    let mut i = l - 1;
+    let mut j = r + 1;
+    let x = &data[indices[(l + r) >> 1]];
+
+    while i < j {
+        loop {
+            i += 1;
+            if data[indices[i]] >= *x {
+                break;
+            }
+        }
+        loop {
+            j -= 1;
+            if data[indices[j]] <= *x {
+                break;
+            }
+        }
+    }
+
+    if k <= j - l + 1 {
+        return quick_select(data, indices, l, j, k);
+    }
+    quick_select(data, indices, j + 1, r, k - (j - l + 1))
+}
+
+#[test]
+fn test_quick_select() {
 }
